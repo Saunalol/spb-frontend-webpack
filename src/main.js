222379856140c8fc7d0c7b294776e36deeca1adb
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-09-02 16:55:15 +0400

    chore: hot module replacement

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ console.log('Test!', require('path'));
 
 var React = require('react');
 var Button = require('./Button.jsx');
+var AppBody = require('./AppBody.jsx');
 
 React.renderComponent(Button({
-    value: 'Hello World!'
-}), document.getElementById('button'));
+        value: 'Hello World!'
+    }),
+    document.getElementById('button'));
+
+React.renderComponent(AppBody(),
+    document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-    entry: './src/main.js',
+    entry: [
+        'webpack/hot/dev-server',
+        './src/main.js'
+    ],
     output: {
         filename: 'src/main.dist.js'
     },
     module: {
         loaders: [{
             test: /\.jsx$/,
-            loader: 'jsx'
+            loaders: [
+                'react-hot',
+                'jsx'
+            ]
         }, {
             test: /\.css$/,
             loader: 'style!css'
